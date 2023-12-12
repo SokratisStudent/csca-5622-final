5c2249f57fca3303d176a0b0467d40a96d67b925
Sokratis Georgiou <soge6802@colorado.edu>
2023-12-12 10:17:39 -0500

    Final?

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from ucimlrepo import fetch_ucirepo
+#from ucimlrepo import fetch_ucirepo
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.ensemble import AdaBoostClassifier, RandomForestClassifier
@@ -8,11 +8,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # fetch dataset
-predict_students_dropout_and_academic_success = fetch_ucirepo(id=697)
+#predict_students_dropout_and_academic_success = fetch_ucirepo(id=697)
 
 # data (as pandas dataframes)
-feature_dataset = predict_students_dropout_and_academic_success.data.features
-target_dataset = predict_students_dropout_and_academic_success.data.targets
+#feature_dataset = predict_students_dropout_and_academic_success.data.features
+#target_dataset = predict_students_dropout_and_academic_success.data.targets
+
+from pandas import read_csv
+feature_dataset = read_csv("ucimlrepo_features.csv")
+target_dataset =  read_csv("ucimlrepo_targets.csv")
 
 combined_dataset = feature_dataset.copy()
 target_column = []
@@ -77,39 +81,49 @@ dt = build_dt(train_features, train_targets, max_depth=current_best_max_depth)
 best_score = evaluate_score(dt, test_features, test_targets)
 print(f'Max-depth tree - Decision Tree with {actual_depth=} and {leaf_nodes=} has precision={current_best_precision:f} and recall={current_best_recall:f} and {best_score=:f}')
 
+precision_list = []
+recall_list = []
 current_best_max_leaf_nodes = 0
-max_max_leaf = 500
+max_max_leaf=200
 
 for leaf in range(2, max_max_leaf):
-    dt = build_dt(train_features, train_targets, max_depth=current_best_max_depth, max_leaf_nodes=leaf)
+    dt = build_dt(train_features, train_targets, max_leaf_nodes=leaf)
     score = evaluate_score(dt, test_features, test_targets)
     if score > current_best_score:
         current_best_score = score
         current_best_max_leaf_nodes = leaf
+    (precision, recall, actual_depth, leaf_nodes) = evaluate(dt, test_features, test_targets)
+    precision_list.append(precision)
+    recall_list.append(recall)
 
 if current_best_max_leaf_nodes == 0:
     current_best_max_leaf_nodes = None
 
 dt = build_dt(train_features, train_targets, max_leaf_nodes=current_best_max_leaf_nodes)
 (current_best_precision, current_best_recall, actual_depth, leaf_nodes) = evaluate(dt, test_features, test_targets)
-best_score = evaluate_score(dt, test_features, test_targets)
-print(f'Pre-pruning tree - Decision Tree with {actual_depth=} and {leaf_nodes=} has precision={current_best_precision:f} and recall={current_best_recall:f} and {best_score=:f}')
+current_best_score = evaluate_score(dt, test_features, test_targets)
+print(f'Pre-pruning tree - Decision Tree with {actual_depth=} and {leaf_nodes=} has precision={current_best_precision:f} and recall={current_best_recall:f} and {current_best_score=:f}')
+
+current_best_max_depth=6
+current_best_max_leaf_nodes=21
 
 path = dt.cost_complexity_pruning_path(train_features, train_targets)
 ccp_alphas, impurities = path.ccp_alphas, path.impurities
-current_best_ccp_alphas = 0.0
+current_best_ccp_alpha = 0.0
 
+clfs = []
 for ccp_alpha in ccp_alphas:
     clf = build_dt(train_features, train_targets, max_depth=current_best_max_depth, max_leaf_nodes=current_best_max_leaf_nodes, ccp_alpha=ccp_alpha)
-    score = evaluate_score(dt, test_features, test_targets)
-    if score > current_best_score:
+    clfs.append(clf)
+    score = evaluate_score(clf, test_features, test_targets)
+    if score >= current_best_score:
         current_best_score = score
-        current_best_ccp_alphas = ccp_alphas
+        current_best_ccp_alpha = ccp_alpha
 
-dt = build_dt(train_features, train_targets, max_depth=current_best_max_depth, max_leaf_nodes=current_best_max_leaf_nodes, ccp_alpha=current_best_ccp_alphas)
+dt = build_dt(train_features, train_targets, max_depth=current_best_max_depth, max_leaf_nodes=current_best_max_leaf_nodes, ccp_alpha=current_best_ccp_alpha)
 (current_best_precision, current_best_recall, actual_depth, leaf_nodes) = evaluate(dt, test_features, test_targets)
 best_score = evaluate_score(dt, test_features, test_targets)
-print(f'Post-pruning tree - Decision Tree with {actual_depth=} and {leaf_nodes=} has precision={current_best_precision:f} and recall={current_best_recall:f} and {best_score=:f}')
+print(f'Post-pruning tree - Decision Tree with {actual_depth=} and {leaf_nodes=} pruned by {current_best_ccp_alpha=} has precision={current_best_precision:f} and recall={current_best_recall:f} and {current_best_score=:f}')
 
 from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, f1_score
 
@@ -129,20 +143,21 @@ print('Enrolled recall:', recall_score(test_targets, test_results, average='micr
 print('Graduate recall:', recall_score(test_targets, test_results, average='micro', labels=['Graduate']))
 
 
-est_search_range = np.arange(5, 55, step=5)
+est_search_range = np.arange(20, 105, step=5)
 lr_search_range = np.linspace(0.1, 1, num=10)
 parameters = {'n_estimators': est_search_range, 'learning_rate': lr_search_range}
 print(parameters)
 
 
-def createAdaBoost(features, labels, n_estimators=50, learning_rate=1.0):
-    return AdaBoostClassifier(n_estimators=n_estimators, learning_rate=learning_rate).fit(features, labels.values.ravel())
+def createAdaBoost(dt, features, labels, n_estimators=50, learning_rate=1.0):
+    return AdaBoostClassifier(dt, n_estimators=n_estimators, learning_rate=learning_rate, random_state=80).fit(features, labels.values.ravel())
 
 
-grid = GridSearchCV(AdaBoostClassifier(estimator=dt), param_grid=parameters, cv=3, verbose=1).fit(train_features, train_targets.values.ravel())
+stump_dt = build_dt(train_features, train_targets, max_depth=1)
+grid = GridSearchCV(createAdaBoost(stump_dt, train_features, train_targets), param_grid=parameters, verbose=5).fit(train_features, train_targets.values.ravel())
 print(grid.best_score_, grid.best_params_)
 
-ada_boost = createAdaBoost(train_features, train_targets, n_estimators=grid.best_params_['n_estimators'], learning_rate=grid.best_params_['learning_rate'])
+ada_boost = createAdaBoost(dt, train_features, train_targets, n_estimators=grid.best_params_['n_estimators'], learning_rate=grid.best_params_['learning_rate'])
 
 print("\nAda-Boosted DT\n")
 test_results = ada_boost.predict(test_features)
@@ -162,19 +177,20 @@ print('Graduate recall:', recall_score(test_targets, test_results, average='micr
 
 # bootstrap is set to false because we have already saved 20% of the dataset for testing so I want the forests to be trained on the entire dataset.
 def build_random_forest(features, labels, max_depth=None, max_leaf_nodes=None, ccp_alpha=0.0):
-    return RandomForestClassifier(bootstrap=False, max_depth=max_depth, max_leaf_nodes=max_leaf_nodes, ccp_alpha=ccp_alpha).fit(features, labels.values.ravel())
+    return RandomForestClassifier(bootstrap=False, max_depth=max_depth, max_leaf_nodes=max_leaf_nodes, ccp_alpha=ccp_alpha, random_state=80).fit(features, labels.values.ravel())
 
 rf = build_random_forest(train_features, train_targets)
 def_score = evaluate_score(rf, test_features, test_targets)
 print(f'Starting point - Random Forest with has {def_score=:f}')
 
-# Running this takes ~60 minutes so I'm just showing the result here.
-#search_range = [ i for i in range(2, 51) ]
-#parameters = {'max_depth': search_range, 'max_leaf_nodes': search_range}
-#grid = GridSearchCV(rf, param_grid=parameters, cv=3, verbose=1).fit(train_features, train_targets.values.ravel())
+# Warning - Running this takes ~60 minutes
+search_range = [ i for i in range(2, 51) ]
+parameters = {'max_depth': search_range, 'max_leaf_nodes': search_range}
+#grid = GridSearchCV(rf, param_grid=parameters, verbose=5).fit(train_features, train_targets.values.ravel())
 #print(grid.best_score_, grid.best_params_)
 print("0.7668837423268786 {'max_depth': 41, 'max_leaf_nodes': 47}")
 
+#rf_grid = build_random_forest(train_features, train_targets, max_depth=grid.best_params_['max_depth'], max_leaf_nodes=grid.best_params_['max_leaf_nodes'])
 rf_grid = build_random_forest(train_features, train_targets, max_depth=41, max_leaf_nodes=47)
 def_score = evaluate_score(rf_grid, test_features, test_targets)
 print(f'After doing grid search - Random Forest with has {def_score=:f}')
