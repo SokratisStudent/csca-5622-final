5c10553f86205df7a48c877c7f9d7ebd0a2a480c
Sokratis Georgiou <soge6802@colorado.edu>
2023-12-11 20:01:13 -0500

    Adding AdaBoost to the mix

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from ucimlrepo import fetch_ucirepo
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.ensemble import AdaBoostClassifier, RandomForestClassifier
 from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, recall_score
 from sklearn.tree import plot_tree, export_text
 import matplotlib.pyplot as plt
+import numpy as np
 
 # fetch dataset
 predict_students_dropout_and_academic_success = fetch_ucirepo(id=697)
@@ -110,14 +111,53 @@ dt = build_dt(train_features, train_targets, max_depth=current_best_max_depth, m
 best_score = evaluate_score(dt, test_features, test_targets)
 print(f'Post-pruning tree - Decision Tree with {actual_depth=} and {leaf_nodes=} has precision={current_best_precision:f} and recall={current_best_recall:f} and {best_score=:f}')
 
+from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, f1_score
 
-def build_random_forest(features, labels, max_depth=None, max_leaf_nodes=None, ccp_alpha=0.0):
-    return RandomForestClassifier(bootstrap=False, max_depth=max_depth, max_leaf_nodes=max_leaf_nodes, ccp_alpha=ccp_alpha).fit(features, labels.values.ravel())
+print("\nDecision Tree\n")
+test_results = dt.predict(test_features)
+print(confusion_matrix(test_targets, test_results), "\n")
+
+print('Overall accuracy:', accuracy_score(test_targets, test_results))
+print('F1_score:', f1_score(test_targets, test_results, average='micro'))
+print()
+print('Dropout precision:', precision_score(test_targets, test_results, average='micro', labels=['Dropout']))
+print('Enrolled precision:', precision_score(test_targets, test_results, average='micro', labels=['Enrolled']))
+print('Graduate precision:', precision_score(test_targets, test_results, average='micro', labels=['Graduate']))
+print()
+print('Dropout recall:', recall_score(test_targets, test_results, average='micro', labels=['Dropout']))
+print('Enrolled recall:', recall_score(test_targets, test_results, average='micro', labels=['Enrolled']))
+print('Graduate recall:', recall_score(test_targets, test_results, average='micro', labels=['Graduate']))
 
 
-rf = build_random_forest(train_features, train_targets)
-def_score = evaluate_score(rf, test_features, test_targets)
-print(f'Starting point - Random Forest with has {def_score=:f}')
+est_search_range = np.arange(5, 55, step=5)
+lr_search_range = np.linspace(0.1, 1, num=10)
+parameters = {'n_estimators': est_search_range, 'learning_rate': lr_search_range}
+print(parameters)
+
+
+def createAdaBoost(features, labels, n_estimators=50, learning_rate=1.0):
+    return AdaBoostClassifier(n_estimators=n_estimators, learning_rate=learning_rate).fit(features, labels.values.ravel())
+
+
+grid = GridSearchCV(AdaBoostClassifier(estimator=dt), param_grid=parameters, cv=3, verbose=1).fit(train_features, train_targets.values.ravel())
+print(grid.best_score_, grid.best_params_)
+
+ada_boost = createAdaBoost(train_features, train_targets, n_estimators=grid.best_params_['n_estimators'], learning_rate=grid.best_params_['learning_rate'])
+
+print("\nAda-Boosted DT\n")
+test_results = ada_boost.predict(test_features)
+print(confusion_matrix(test_targets, test_results), "\n")
+
+print('Overall accuracy:', accuracy_score(test_targets, test_results))
+print('F1_score:', f1_score(test_targets, test_results, average='micro'))
+print()
+print('Dropout precision:', precision_score(test_targets, test_results, average='micro', labels=['Dropout']))
+print('Enrolled precision:', precision_score(test_targets, test_results, average='micro', labels=['Enrolled']))
+print('Graduate precision:', precision_score(test_targets, test_results, average='micro', labels=['Graduate']))
+print()
+print('Dropout recall:', recall_score(test_targets, test_results, average='micro', labels=['Dropout']))
+print('Enrolled recall:', recall_score(test_targets, test_results, average='micro', labels=['Enrolled']))
+print('Graduate recall:', recall_score(test_targets, test_results, average='micro', labels=['Graduate']))
 
 
 # bootstrap is set to false because we have already saved 20% of the dataset for testing so I want the forests to be trained on the entire dataset.
@@ -128,10 +168,9 @@ rf = build_random_forest(train_features, train_targets)
 def_score = evaluate_score(rf, test_features, test_targets)
 print(f'Starting point - Random Forest with has {def_score=:f}')
 
-search_range = [ i for i in range(2, 51) ]
-parameters = {'max_depth': search_range, 'max_leaf_nodes': search_range}
-
 # Running this takes ~60 minutes so I'm just showing the result here.
+#search_range = [ i for i in range(2, 51) ]
+#parameters = {'max_depth': search_range, 'max_leaf_nodes': search_range}
 #grid = GridSearchCV(rf, param_grid=parameters, cv=3, verbose=1).fit(train_features, train_targets.values.ravel())
 #print(grid.best_score_, grid.best_params_)
 print("0.7668837423268786 {'max_depth': 41, 'max_leaf_nodes': 47}")
@@ -140,29 +179,12 @@ rf_grid = build_random_forest(train_features, train_targets, max_depth=41, max_l
 def_score = evaluate_score(rf_grid, test_features, test_targets)
 print(f'After doing grid search - Random Forest with has {def_score=:f}')
 
-
-
-
-
-print("\nDecision Tree\n")
-test_results = dt.predict(test_features)
-print(confusion_matrix(test_targets, test_results), "\n")
-
-print('Overall accuracy:', accuracy_score(test_targets, test_results))
-print()
-print('Dropout precision:', precision_score(test_targets, test_results, average='micro', labels=['Dropout']))
-print('Enrolled precision:', precision_score(test_targets, test_results, average='micro', labels=['Enrolled']))
-print('Graduate precision:', precision_score(test_targets, test_results, average='micro', labels=['Graduate']))
-print()
-print('Dropout recall:', recall_score(test_targets, test_results, average='micro', labels=['Dropout']))
-print('Enrolled recall:', recall_score(test_targets, test_results, average='micro', labels=['Enrolled']))
-print('Graduate recall:', recall_score(test_targets, test_results, average='micro', labels=['Graduate']))
-
 print("\nRandom Forest\n")
 test_results = rf.predict(test_features)
 print(confusion_matrix(test_targets, test_results), "\n")
 
 print('Overall accuracy:', accuracy_score(test_targets, test_results))
+print('F1_score:', f1_score(test_targets, test_results, average='micro'))
 print()
 print('Dropout precision:', precision_score(test_targets, test_results, average='micro', labels=['Dropout']))
 print('Enrolled precision:', precision_score(test_targets, test_results, average='micro', labels=['Enrolled']))
@@ -171,7 +193,6 @@ print()
 print('Dropout recall:', recall_score(test_targets, test_results, average='micro', labels=['Dropout']))
 print('Enrolled recall:', recall_score(test_targets, test_results, average='micro', labels=['Enrolled']))
 print('Graduate recall:', recall_score(test_targets, test_results, average='micro', labels=['Graduate']))
-
 
 exp = export_text(dt, feature_names=feature_dataset.columns)
 with open('tree_export', 'w') as f:
